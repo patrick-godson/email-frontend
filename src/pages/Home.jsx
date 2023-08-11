0254ca9c0548fdcf2dbc2ee7799f6f6c5efb8b8f
patrick-godson <58881396+patrick-godson@users.noreply.github.com>
2023-08-11 10:23:36 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 
 export default function Home(){
-    const [total, setTotal] = React.useState(0);
-    const [totalUnread, setUnread] = React.useState(0);
+    const [total, setTotal] = React.useState(null);
+    const [totalUnread, setUnread] = React.useState(null);
 
     React.useEffect(() =>{
         fetch('https://emailbox-api-x0rw.onrender.com', {mode: "no-cors"})
@@ -17,7 +17,7 @@ export default function Home(){
           <div className='homeContent'>
             <h1>Hello <span className='spanName'>Patson</span></h1>
             <h2>
-              You have {totalUnread} unread messages out of {total}
+              You have {totalUnread && totalUnread} unread messages out of {total && total}
             </h2>
             <a href="https://emailbox-9n1z.onrender.com/inbox" >View Messages</a>
           </div>
