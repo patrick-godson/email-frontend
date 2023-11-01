09bf1e9e1c5451b60e78a06d5b7a23844ce331f5
patrick-godson <58881396+patrick-godson@users.noreply.github.com>
2023-11-01 17:24:11 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home(){
     const [data, setData] = useState(null);
 
     useEffect(() =>{
-        fetch('http://localhost:3500')
+        fetch('https://emailbox-api-1101.onrender.com')
         .then(res => res.json())
         .then(data => {setData(data);});
     }, []);
